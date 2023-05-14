0d8611755cf21200d72e0b0e07469cd60c5b58e8
Steeven Salony <steeven@iMac.local>
2023-05-13 23:42:41 -0400

    fix connection to db

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
+console.log(process.env)
 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306
     });
 
 module.exports = sequelize;
